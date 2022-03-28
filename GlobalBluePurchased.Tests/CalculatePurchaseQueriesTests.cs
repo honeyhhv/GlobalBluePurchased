ceca9e8e04c58e8a1799fa291a671d875c9c3682
Milad <rajabi.milad@gmail.com>
2022-03-28 12:42:14 +0430

    .

--- GlobalBluePurchased.Tests/CalculatePurchaseQueriesTests.cs
+++ GlobalBluePurchased.Tests/CalculatePurchaseQueriesTests.cs
@@ -14,6 +14,7 @@ namespace GlobalBluePurchased.Tests
     {
         private CalculatePurchaseQueriesHandler _handler;
         private CalculatePurchaseValidator _validator;
+        
         [SetUp]
         public void Setup()
         {
